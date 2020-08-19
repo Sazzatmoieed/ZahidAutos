2b3287be1567b945fe86c1e4c7644f7b7d2aee4d
Sazzatmoieed <69335875+Sazzatmoieed@users.noreply.github.com>
2020-08-19 15:17:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1500,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
